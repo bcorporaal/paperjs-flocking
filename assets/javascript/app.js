d32e689934732a3b2a8c13e094161a7f948ac2fe
bcorporaal <dev@reefscape.net>
2017-08-02 00:20:42 +0200

    change description

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,9 @@
 //
-//  This file is used by Codekit to combine multiple javascript files into a single file.
+//  This statements below are used by Codekit to combine multiple javascript files into a single file.
 //  See https://codekitapp.com for more information.
 //
-//  If you don't use Codekit you can ignore this file and use the separate javascript files.
-//  Add them in the html source in the same order as below.
+//  If you don't use Codekit you can ignore this and use the separate javascript files.
+//  Include them in the html source in the same order as below.
 
 // @codekit-prepend "core.js";
 // @codekit-prepend "boid.js";
